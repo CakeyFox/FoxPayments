a874efe5c76f932450fc0c67729c86fcf1c407cd
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-10-19 12:02:26 -0300

    Send information about the purchased plan to the end user

--- services/foxy/OrderHandler.ts
+++ services/foxy/OrderHandler.ts
@@ -1,3 +1,4 @@
+import { database } from "../..";
 import { MercadoPagoOrder } from "../../utils/types/order";
 import { RestManager } from "./RestManager";
 
@@ -8,19 +9,24 @@ export default class OrderHandler {
         this.rest = new RestManager();
     }
 
-    async createOrder(order: MercadoPagoOrder) {
-        this.rest.sendDirectMessage(order.data.id, {
-            embeds: [{
-                title: "Obrigada por me ajudar a ficar online, yay!",
-                description: "Muito obrigada por comprar o premium! Você não sabe o quanto isso me ajuda (a comprar comida) e me manter online, você é incrível! 💜",
-                color: 0xe7385d,
-                image: {
-                    url: "https://cdn.discordapp.com/attachments/1078322762550083736/1160621235525386240/tier1.png?ex=671292c1&is=67114141&hm=1b3dc9a6bd96a211236fba4d78818ff466ffbf9bba115cb91b4663e49d3fd5a5&"
-                },
-                footer: {
-                    text: "Obrigada por me ajudar a ficar online!"
-                }
-            }]
+    async createOrder(order: MercadoPagoOrder, userId: string, itemId: string) {
+        const itemInfo = await database.getProductFromStore(itemId);
+        const checkoutInfo = await database.getCheckoutByUserId(userId);
+        
+        const embed = {
+            title: "Obrigada por me ajudar a ficar online, yay!",
+            description: `Muito obrigada por comprar o **${itemInfo.itemName}**! Você não sabe o quanto isso me ajuda (a comprar comida) e me manter online, você é incrível! 💜`,
+            color: 0xe7385d,
+            image: {
+                url: "https://cdn.discordapp.com/attachments/1078322762550083736/1160621235525386240/tier1.png?ex=671292c1&is=67114141&hm=1b3dc9a6bd96a211236fba4d78818ff466ffbf9bba115cb91b4663e49d3fd5a5&"
+            },
+            footer: {
+                text: "Obrigada por me ajudar a ficar online!"
+            }
+        };
+
+        this.rest.sendDirectMessage(userId, {
+            embeds: [embed]
         });
     }
 }
